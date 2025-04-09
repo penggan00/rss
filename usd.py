d43733d2239eabc3c910c81479d5bcd5c3850891
好酒 <106035465+penggan00@users.noreply.github.com>
2025-04-09 10:22:04 +0800

    Update usd.py

--- usd.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-import requests
-import os
-from dotenv import load_dotenv
-import time
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from datetime import datetime, timedelta
-import pytz
-from lunarcalendar import Converter, Solar, Lunar
